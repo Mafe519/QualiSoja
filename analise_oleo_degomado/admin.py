0e9514017aeb06750877320f4bc59ad5d7f08379
ogeorgehenrique <engenheiro.georgehenrique@gmail.com>
2025-06-07 15:34:45 -0400

    UPDATE: tabelas do Banco de Dados para Analise de oleo Degoamado, e criação da interface HTML

--- analise_oleo_degomado/admin.py
+++ analise_oleo_degomado/admin.py
@@ -1,14 +1,21 @@
 from django.contrib import admin
 
-from analise_oleo_degomado.models import OleoDegomado, TipoOleo
+from analise_oleo_degomado.models import AnaliseOleoDegomado
 
-class TipoOleoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'hora')
-    search_fields = ('nome',)
+##Ñ usar!!!
+# class TipoOleoAdmin(admin.ModelAdmin):
+#     list_display = ('nome', 'hora')
+#     search_fields = ('nome',)
 
-class OleoDegomadoAdmin(admin.ModelAdmin):
-    list_display = ('umidade', 'tara', 'liquido', 'peso_amostra', 'resultado')
-    search_fields = ('umidade',)
+# class OleoDegomadoAdmin(admin.ModelAdmin):
+#     list_display = ('umidade', 'tara', 'liquido', 'peso_amostra', 'resultado')
+#     search_fields = ('umidade',)
 
-admin.site.register(OleoDegomado, OleoDegomadoAdmin)
-admin.site.register(TipoOleo, TipoOleoAdmin)
+# admin.site.register(OleoDegomado, OleoDegomadoAdmin)
+# admin.site.register(TipoOleo, TipoOleoAdmin)
+
+@admin.register(AnaliseOleoDegomado)
+class AnaliseOleoDegomadoAdmin(admin.ModelAdmin):
+    list_display = ['data', 'horario', 'tipo_amostra', 'peso_amostra', 'resultado']
+    list_filter = ['tipo_amostra', 'data']
+    search_fields = ['tipo_amostra']
